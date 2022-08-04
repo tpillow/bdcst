bc5d6c51b04c9641a9bc9c08e131dddaa158e7c8
CodeNMore <jackofall55@gmail.com>
2022-08-03 23:09:31 -0400

    Updated unit tests

--- pkg/bdcst/broadcaster_test.go
+++ pkg/bdcst/broadcaster_test.go
@@ -6,33 +6,39 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBroadcasterNoListeners(t *testing.T) {
+func TestBroadcasterNoListenersSendDoesntBlock(t *testing.T) {
 	br := NewBroadcaster[int]()
-	assert.Equal(t, 0, len(br.listeners))
 	br.Send(10)
 }
 
-func TestBroadcasterCallbackListener(t *testing.T) {
+func TestBroadcasterAddingListenersIncreasesNumListeners(t *testing.T) {
 	br := NewBroadcaster[int]()
-	br.Send(10)
+	assert.Equal(t, 0, br.NumListeners())
+	br.AddListener(NewChannelListener(make(chan int)))
+	assert.Equal(t, 1, br.NumListeners())
+	br.AddListener(NewChannelListener(make(chan int)))
+	assert.Equal(t, 2, br.NumListeners())
+}
+
+func TestBroadcasterCallbackListenerReceivesData(t *testing.T) {
+	br := NewBroadcaster[int]()
 	complete := make(chan int, 1)
 	br.AddListener(NewCallbackListener(func(data int) {
 		complete <- data
 	}))
-	br.Send(20)
-	assert.Equal(t, 20, <-complete)
+	br.Send(10)
+	assert.Equal(t, 10, <-complete)
 }
 
-func TestBroadcasterChannelListener(t *testing.T) {
+func TestBroadcasterChannelListenerReceivesData(t *testing.T) {
 	br := NewBroadcaster[int]()
-	br.Send(10)
 	complete := make(chan int, 1)
 	br.AddListener(NewChannelListener(complete))
-	br.Send(20)
-	assert.Equal(t, 20, <-complete)
+	br.Send(10)
+	assert.Equal(t, 10, <-complete)
 }
 
-func TestBroadcasterCallbackAndChannelListener(t *testing.T) {
+func TestBroadcasterCallbackAndChannelListenerReceiveData(t *testing.T) {
 	br := NewBroadcaster[int]()
 	fComplete := make(chan int, 1)
 	cComplete := make(chan int, 1)
@@ -40,15 +46,17 @@ func TestBroadcasterCallbackAndChannelListener(t *testing.T) {
 		fComplete <- data
 	}))
 	br.AddListener(NewChannelListener(cComplete))
+
 	br.Send(10)
 	assert.Equal(t, 10, <-fComplete)
 	assert.Equal(t, 10, <-cComplete)
+
 	br.Send(20)
 	assert.Equal(t, 20, <-fComplete)
 	assert.Equal(t, 20, <-cComplete)
 }
 
-func TestBroadcasterRemoveInvalidListener(t *testing.T) {
+func TestBroadcasterRemoveUnaddedListener(t *testing.T) {
 	br := NewBroadcaster[int]()
 	assert.Panics(t, func() {
 		br.RemoveListener(NewCallbackListener(func(data int) {}))
@@ -62,34 +70,89 @@ func TestBroadcasterRemoveCallbackListener(t *testing.T) {
 		complete <- data
 	})
 	br.AddListener(cbl)
-	assert.Equal(t, 1, br.NumListeners())
+
 	br.Send(10)
 	assert.Equal(t, 10, <-complete)
 
 	br.RemoveListener(cbl)
 	assert.Equal(t, 0, br.NumListeners())
+
+	br.Send(20)
+	select {
+	case <-complete:
+		assert.Fail(t, "Unexpected data received by a listener")
+	default:
+	}
+}
+
+func TestBroadcasterRemoveChannelListener(t *testing.T) {
+	br := NewBroadcaster[int]()
+	complete := make(chan int, 1)
+	cbl := NewChannelListener(complete)
+	br.AddListener(cbl)
+
+	br.Send(10)
+	assert.Equal(t, 10, <-complete)
+
+	br.RemoveListener(cbl)
+	assert.Equal(t, 0, br.NumListeners())
+
+	br.Send(20)
+	select {
+	case <-complete:
+		assert.Fail(t, "Unexpected data received by a listener")
+	default:
+	}
+}
+
+func TestBroadcasterTwoListenersRemoveCallbackListener(t *testing.T) {
+	br := NewBroadcaster[int]()
+	complete := make(chan int, 1)
+	cbl := NewCallbackListener(func(data int) {
+		complete <- data
+	})
+	br.AddListener(cbl)
+	complete2 := make(chan int, 1)
+	cbl2 := NewChannelListener(complete2)
+	br.AddListener(cbl2)
+
+	br.Send(10)
+	assert.Equal(t, 10, <-complete)
+	assert.Equal(t, 10, <-complete2)
+
+	br.RemoveListener(cbl)
+
+	br.Send(20)
+	assert.Equal(t, 20, <-complete2)
+	select {
+	case <-complete:
+		assert.Fail(t, "Unexpected data received by a listener")
+	default:
+	}
 }
 
 func TestBroadcasterTwoListenersRemoveChannelListener(t *testing.T) {
 	br := NewBroadcaster[int]()
 	complete := make(chan int, 1)
+	cbl := NewCallbackListener(func(data int) {
+		complete <- data
+	})
+	br.AddListener(cbl)
 	complete2 := make(chan int, 1)
-	listener := NewChannelListener(complete)
-	br.AddListener(listener)
-	assert.Equal(t, 1, len(br.listeners))
-	br.AddListener(NewChannelListener(complete2))
-	assert.Equal(t, 2, len(br.listeners))
+	cbl2 := NewChannelListener(complete2)
+	br.AddListener(cbl2)
+
 	br.Send(10)
 	assert.Equal(t, 10, <-complete)
 	assert.Equal(t, 10, <-complete2)
 
-	br.RemoveListener(listener)
-	assert.Equal(t, 1, len(br.listeners))
+	br.RemoveListener(cbl2)
+
 	br.Send(20)
-	assert.Equal(t, 20, <-complete2)
+	assert.Equal(t, 20, <-complete)
 	select {
-	case <-complete:
-		assert.Fail(t, "Unexpected data sent to removed listener")
+	case <-complete2:
+		assert.Fail(t, "Unexpected data received by a listener")
 	default:
 	}
 }
