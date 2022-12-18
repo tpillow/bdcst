ad86196f3c65615ee6181c249aff6f5616661cb8
CodeNMore <jackofall55@gmail.com>
2022-12-18 17:30:38 -0500

    Simplified val broadcaster

--- pkg/bdcst/val_broadcaster.go
+++ pkg/bdcst/val_broadcaster.go
@@ -1,31 +1,27 @@
 package bdcst
 
 type ValBroadcaster[T any] struct {
-	broadcaster *Broadcaster[T]
-	value       T
+	Broadcaster[T]
+	value T
 }
 
 // Returns a new Broadcaster[T] with the provided listeners pre-added.
-func NewValBroadcaster[T any](val T, listeners ...Listener[T]) *ValBroadcaster[T] {
+func NewValBroadcaster[T any](value T, listeners ...Listener[T]) *ValBroadcaster[T] {
 	return &ValBroadcaster[T]{
-		broadcaster: NewBroadcaster[T](listeners...),
-		value:       val,
+		Broadcaster[T]{listeners: listeners},
+		value,
 	}
 }
 
 func (vb *ValBroadcaster[T]) SetValue(value T) {
-	vb.broadcaster.mutex.Lock()
-	defer vb.broadcaster.mutex.Unlock()
+	vb.mutex.Lock()
+	defer vb.mutex.Unlock()
 	vb.value = value
-	vb.broadcaster.notifyNoLock(vb.value)
+	vb.notifyNoLock(vb.value)
 }
 
 func (vb *ValBroadcaster[T]) GetValue() T {
-	vb.broadcaster.mutex.Lock()
-	defer vb.broadcaster.mutex.Unlock()
+	vb.mutex.Lock()
+	defer vb.mutex.Unlock()
 	return vb.value
-}
-
-func (vb *ValBroadcaster[T]) GetBroadcaster() *Broadcaster[T] {
-	return vb.broadcaster
 }
