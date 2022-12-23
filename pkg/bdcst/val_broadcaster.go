6f58e7d5ae821b648dcf7958fc30fdd11d8d25ef
CodeNMore <jackofall55@gmail.com>
2022-12-22 20:22:27 -0500

    Added notify setting if same value setting

--- pkg/bdcst/val_broadcaster.go
+++ pkg/bdcst/val_broadcaster.go
@@ -1,21 +1,26 @@
 package bdcst
 
-type ValBroadcaster[T any] struct {
+type ValBroadcaster[T comparable] struct {
 	Broadcaster[T]
-	value T
+	value                  T
+	notifySettingSameValue bool
 }
 
 // Returns a new Broadcaster[T] with the provided listeners pre-added.
-func NewValBroadcaster[T any](value T, listeners ...Listener[T]) *ValBroadcaster[T] {
+func NewValBroadcaster[T comparable](value T, notifySettingSameValue bool, listeners ...Listener[T]) *ValBroadcaster[T] {
 	return &ValBroadcaster[T]{
 		Broadcaster[T]{listeners: listeners},
 		value,
+		notifySettingSameValue,
 	}
 }
 
 func (vb *ValBroadcaster[T]) SetValue(value T) {
 	vb.mutex.Lock()
 	defer vb.mutex.Unlock()
+	if !vb.notifySettingSameValue && vb.value == value {
+		return
+	}
 	vb.value = value
 	vb.notifyNoLock(vb.value)
 }
